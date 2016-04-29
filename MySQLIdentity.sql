fff42b923e82c49e73468cb84024fb35ad79d37c
August Burns <shturm@urghh.net>
2016-04-29 18:20:23 +0300

    Make SQL init script compliant with table classes

--- MySQLIdentity.sql
+++ MySQLIdentity.sql
@@ -1,40 +1,40 @@
-CREATE TABLE `<DATABASENAME>`.`users` (
+CREATE TABLE `<DATABASENAME>`.`Users` (
   `Id` VARCHAR(45) NOT NULL,
   `UserName` VARCHAR(45) NULL,
   `PasswordHash` VARCHAR(100) NULL,
   `SecurityStamp` VARCHAR(45) NULL,
   PRIMARY KEY (`id`));
 
-CREATE TABLE `<DATABASENAME>`.`roles` (
+CREATE TABLE `<DATABASENAME>`.`Roles` (
   `Id` VARCHAR(45) NOT NULL,
   `Name` VARCHAR(45) NULL,
   PRIMARY KEY (`Id`));
 
-CREATE TABLE `<DATABASENAME>`.`userclaims` (
+CREATE TABLE `<DATABASENAME>`.`UserClaims` (
   `Id` INT NOT NULL AUTO_INCREMENT,
   `UserId` VARCHAR(45) NULL,
   `ClaimType` VARCHAR(100) NULL,
   `ClaimValue` VARCHAR(100) NULL,
   PRIMARY KEY (`Id`),
   FOREIGN KEY (`UserId`)
-    REFERENCES `<DATABASENAME>`.`users` (`Id`) on delete cascade);
+    REFERENCES `<DATABASENAME>`.`Users` (`Id`) on delete cascade);
 
-CREATE TABLE `<DATABASENAME>`.`userlogins` (
+CREATE TABLE `<DATABASENAME>`.`UserLogins` (
   `UserId` VARCHAR(45) NOT NULL,
   `ProviderKey` VARCHAR(100) NULL,
   `LoginProvider` VARCHAR(100) NULL,
   FOREIGN KEY (`UserId`)
-    REFERENCES `<DATABASENAME>`.`users` (`Id`) on delete cascade);
+    REFERENCES `<DATABASENAME>`.`Users` (`Id`) on delete cascade);
 
-CREATE TABLE `<DATABASENAME>`.`userroles` (
+CREATE TABLE `<DATABASENAME>`.`UserRoles` (
   `UserId` VARCHAR(45) NOT NULL,
   `RoleId` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`UserId`, `RoleId`),
   FOREIGN KEY (`UserId`)
-    REFERENCES `<DATABASENAME>`.`users` (`Id`) 
+    REFERENCES `<DATABASENAME>`.`Users` (`Id`) 
 		on delete cascade
 		on update cascade,
   FOREIGN KEY (`RoleId`)
-    REFERENCES `<DATABASENAME>`.`roles` (`Id`)
+    REFERENCES `<DATABASENAME>`.`Roles` (`Id`)
 		on delete cascade
 		on update cascade);
